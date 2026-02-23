be7e3ad33f1161ba446c784c7456d73e1224312c
Josue Rodriguez <josuerodcat90@gmail.com>
2026-02-24 00:03:35 +0100

    first class completed

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,15 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
-  ]
+	providers: [
+		provideBrowserGlobalErrorListeners(),
+		provideRouter(routes, withComponentInputBinding()),
+		provideHttpClient(withFetch()),
+		provideClientHydration(withEventReplay()),
+	],
 };
